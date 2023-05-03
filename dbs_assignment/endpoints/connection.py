4f2f91a5f25b47f9bc222485d3d1b3c439ae5f51
Filip Zatroch <91148237+Vapora293@users.noreply.github.com>
2023-05-03 21:54:12 +0200

    categories requests added

--- dbs_assignment/endpoints/connection.py
+++ dbs_assignment/endpoints/connection.py
@@ -1,3 +1,4 @@
+import sqlalchemy.exc
 from fastapi import APIRouter, HTTPException
 from fastapi import Body
 
@@ -8,10 +9,9 @@ from pydantic import ValidationError
 
 from dbs_assignment import schemas
 from dbs_assignment.config import engine
-from dbs_assignment.models import Author
+from dbs_assignment.models import Author, Category
 from typing import Any
 import uuid
-
 
 router = APIRouter()
 
@@ -22,12 +22,15 @@ async def create_publication(payload: schemas.Publication):
 
 
 def sql_execution(fetching, deleteFlag=False):
-    with engine.connect() as conn:
-        result = conn.execute(fetching)
-        conn.commit()
-        if deleteFlag is True:
-            return result.rowcount
-        return result.fetchone()
+    try:
+        with engine.connect() as conn:
+            result = conn.execute(fetching)
+            conn.commit()
+            if deleteFlag is True:
+                return result.rowcount
+    except sqlalchemy.exc.DataError:
+        raise HTTPException(status_code=400)
+    return result.fetchone()
 
 
 def author_return(record):
@@ -87,6 +90,68 @@ def update_author(author_id: str, payload: schemas.AuthorUpdateSchema) -> Any:
 @router.delete("/authors/{author_id}", status_code=204)
 def delete_author(author_id: str):
     fetching = delete(Author).where(Author.id == author_id)
+    result = sql_execution(fetching, True)
+    if result == 0:
+        raise HTTPException(status_code=400)
+    return None
+
+
+def category_return(record):
+    return {
+        "id": record[0],
+        "name": record[1],
+        "createdAt": record[2],
+        "updatedAt": record[3]
+    }
+
+
+@router.post("/categories", status_code=201)
+def create_category(payload: dict = Body(...)) -> Any:
+    try:
+        category_schema = schemas.CategorySchema(**payload)
+    except ValidationError:
+        raise HTTPException(status_code=400)
+
+    if category_schema.id is None:
+        category_schema.id = str(uuid.uuid4())
+    category_data = category_schema.dict()
+
+    try:
+        fetching = insert(Category).values(**category_data).returning(Category.id, Category.name,
+                                                                      Category.createdAt, Category.updatedAt)
+        record = sql_execution(fetching)
+    except IntegrityError:
+        raise HTTPException(status_code=409)
+
+    return category_return(record)
+
+
+@router.get("/categories/{category_id}", status_code=200)
+def get_category(category_id: str):
+    fetching = select(Category).where(Category.id == category_id)
+    record = sql_execution(fetching)
+    if not record:
+        raise HTTPException(status_code=404)
+
+    return category_return(record)
+
+
+@router.patch("/categories/{category_id}", status_code=200)
+def update_category(category_id: str, payload: schemas.CategoryUpdateSchema) -> Any:
+    update_data = payload.dict(exclude_unset=True)
+    if not update_data:
+        raise HTTPException(status_code=400)
+    fetching = update(Category).where(Category.id == category_id).values(**update_data).returning(Category.id,
+                                                                                                  Category.name,
+                                                                                                  Category.createdAt,
+                                                                                                  Category.updatedAt)
+
+    return category_return(sql_execution(fetching))
+
+
+@router.delete("/categories/{category_id}", status_code=204)
+def delete_author(category_id: str):
+    fetching = delete(Category).where(Category.id == category_id)
     result = sql_execution(fetching, True)
     if result == 0:
         raise HTTPException(status_code=400)
--- dbs_assignment/schemas.py
+++ dbs_assignment/schemas.py
@@ -1,23 +1,7 @@
-import uuid
-
 from pydantic import BaseModel, Field
 from uuid import UUID, uuid4
 from typing import Optional
 
-
-# class NoteBaseSchema(BaseModel):
-#     id: str | None = None
-#     title: str
-#     content: str
-#     category: str | None = None
-#     published: bool = False
-#     createdAt: datetime | None = None
-#     updatedAt: datetime | None = None
-#
-#     class Config:
-#         orm_mode = True
-#         allow_population_by_field_name = True
-#         arbitrary_types_allowed = True
 
 class AuthorSchema(BaseModel):
     id: UUID = Field(default_factory=uuid4)
@@ -31,6 +15,18 @@ class AuthorSchema(BaseModel):
 class AuthorUpdateSchema(BaseModel):
     name: Optional[str] = None
     surname: Optional[str] = None
+
+
+class CategorySchema(BaseModel):
+    id: UUID = Field(default_factory=uuid4)
+    name: str
+
+    class Config:
+        orm_mode = True
+
+
+class CategoryUpdateSchema(BaseModel):
+    name: Optional[str] = None
 
 
 class Publication(BaseModel):
